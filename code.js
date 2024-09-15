48bd06fdf3802028d32d105df4e0c38cf176d0f5
Russo <aleksander@ice.ufjf.br>
2024-09-15 19:25:13 -0300

    replace instances by clones

--- code.js
+++ code.js
@@ -5,57 +5,63 @@ const pluginName = "Map loop";
 
 const dummy = new THREE.Object3D();
 
-setInterval(function ()
+function traverse(mesh, pos = new THREE.Vector3(0, 0, 0), rot = new THREE.Euler(0, 0, 0, "XYZ"), scale = new THREE.Vector3(1, 1, 1))
 {
-    if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
-    {
-		const x = RPM.Datas.Systems.SQUARE_SIZE * RPM.Scene.Map.current.mapProperties.length;
-		const z = RPM.Datas.Systems.SQUARE_SIZE * RPM.Scene.Map.current.mapProperties.width;
-		const s = RPM.Scene.Map.current.scene;
-		for (var i = 0; i < s.children.length; i++)
+	if (mesh === RPM.Core.Game.current.hero.mesh || mesh.mapLoopPlugin_isClone)
+		return;
+	if (mesh.isMesh)
+	{
+		const m = RPM.Scene.Map.current;
+		const x = RPM.Datas.Systems.SQUARE_SIZE * m.mapProperties.length;
+		const z = RPM.Datas.Systems.SQUARE_SIZE * m.mapProperties.width;
+		if (!mesh.mapLoopPlugin_hasClone)
 		{
-			console.log(s.children[i]);
-			if (!s.children[i].isMesh)
-				continue;
-			const mesh = s.children[i];
-			if (!mesh.mapLoopPlugin_instancedMesh)
+			const inst = [];
+			for (var i = 0; i < 8; i++)
 			{
-				const geo = mesh.geometry;
-				const mat = mesh.material;
-				const inst = new THREE.InstancedMesh(geo, mat, 8);
-				inst.castShadow = true;
-				inst.receiveShadow = true;
-				inst.customDepthMaterial = mesh.customDepthMaterial;
-				inst.customDistanceMaterial = mesh.customDistanceMaterial;
-				mesh.mapLoopPlugin_instancedMesh = inst;
-				console.log(inst);
-//				s.add(inst);
+				inst.push(mesh.clone(false));
+				inst[i].morphTargetInfluences = mesh.morphTargetInfluences;
+				inst[i].mapLoopPlugin_isClone = true;
+				m.scene.add(inst[i]);
 			}
-			else
+			mesh.mapLoopPlugin_hasClone = inst;
+		}
+		const inst = mesh.mapLoopPlugin_hasClone;
+		var i = 0;
+		for (var j = 0; j < 3; j++)
+		{
+			for (var k = 0; k < 3; k++)
 			{
-				const geo = mesh.geometry;
-				const mat = mesh.material;
-				const inst = mesh.mapLoopPlugin_instancedMesh;
-				var count = 0;
-				for (var j = 0; j < 3; j++)
-				{
-					for (var k = 0; k < 3; k++)
-					{
-						if (j * 3 + k === 4)
-							continue;
-						dummy.position.set((1 - j) * x + mesh.position.x, mesh.position.y, (1 - k) * z + mesh.position.z);
-						dummy.rotation.copy(mesh.rotation);
-						dummy.updateMatrix();
-						inst.setMatrixAt(count++, dummy.matrix);
-					}
-				}
-				inst.instanceMatrix.needsUpdate = true;
+				if (j * 3 + k === 4)
+					continue;
+				inst[i].position.set((1 - j) * x, 0, (1 - k) * z);
+				inst[i].rotation.copy(mesh.rotation);
+				inst[i].scale.copy(mesh.scale);
+				i++;
 			}
 		}
+	}
+	pos.add(mesh.position);
+	rot.set(rot.x + mesh.rotation.x, rot.y + mesh.rotation.y, rot.z + mesh.rotation.z, "XYZ");
+	scale.multiplyVectors(scale, mesh.scale);
+	for (var i = 0; i < mesh.children.length; i++)
+		traverse(mesh.children[i], pos, rot, scale);
+}
+
+setInterval(function ()
+{
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
+	{
+		const m = RPM.Scene.Map.current;
+		if (m.mapLoopPlugin_loopX || m.mapLoopPlugin_loopZ)
+			for (var i = 0; i < m.scene.children.length; i++)
+				traverse(m.scene.children[i]);
 	}
 }, 16);
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Set map loop", (x, z) =>
 {
-	
+	const m = RPM.Scene.Map.current;
+	m.mapLoopPlugin_loopX = x;
+	m.mapLoopPlugin_loopZ = z;
 });
